0136e53e393843616793ab87222eb689fe861ddb
swfeathers <swfeathers@gmail.com>
2018-09-06 21:27:54 +0100

    Update feathers-player.js

--- js/feathers-player.js
+++ js/feathers-player.js
@@ -6,25 +6,25 @@ elms.forEach(function(elm) {
 
 function loadAll() {
 var ipDemo = new Howl({
-  src: ['../sounds/ip-Demo.mp3'],
+  src: ['../sounds/ip-demo.mp3'],
   preload: true,
   loop: true,
   volume: 1.0
 });
 var ipMaster = new Howl({
-  src: ['../sounds/ip-Master.mp3'],
+  src: ['../sounds/ip-master.mp3'],
   preload: true,
   loop: true,
   volume: 0.0
 });
 var spacesDemo = new Howl({
-  src: ['../sounds/spaces-Demo.mp3'],
+  src: ['../sounds/spaces-demo.mp3'],
   preload: true,
   loop: true,
   volume: 1.0
 });
 var spacesMaster = new Howl({
-  src: ['../sounds/spaces-Master.mp3'],
+  src: ['../sounds/spaces-master.mp3'],
   preload: true,
   loop: true,
   volume: 0.0
