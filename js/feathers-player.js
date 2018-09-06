60208e2ede18ead26de302834d5c71ad3cde5621
swfeathers <swfeathers@gmail.com>
2018-09-06 20:58:24 +0100

    Create feathers-player.js

---
+++ js/feathers-player.js
@@ -0,0 +1,42 @@
+var elms = ['ipPlayBtn', 'spacesPlayBtn', 'ipDemoRadio', 'ipMasterRadio', 'spacesDemoRadio', 'spacesMasterRadio'];
+
+elms.forEach(function(elm) {
+  window[elm] = document.getElementById(elm);
+});
+
+function loadAll() {
+var ipDemo = new Howl({
+  src: ['../sounds/ipDemo.mp3'],
+  preload: true,
+  loop: true,
+  volume: 1.0
+});
+var ipMaster = new Howl({
+  src: ['../sounds/ipMaster.mp3'],
+  preload: true,
+  loop: true,
+  volume: 0.0
+});
+var spacesDemo = new Howl({
+  src: ['../sounds/spacesDemo.mp3'],
+  preload: true,
+  loop: true,
+  volume: 1.0
+});
+var spacesMaster = new Howl({
+  src: ['../sounds/spacesMaster.mp3'],
+  preload: true,
+  loop: true,
+  volume: 0.0
+});
+};
+
+ipPlayBtn.addEventListener('click', function() {
+  ipDemo.play()
+  ipMaster.play()
+});
+
+spacesPlayBtn.addEventListener('click', function() {
+  ipDemo.play()
+  ipMaster.play()
+});
